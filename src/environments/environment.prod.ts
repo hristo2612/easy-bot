469004516aceb5e8c391e3f7dd72d8b9a65f9009
hristo2612 <hristoapps@gmail.com>
2019-05-22 14:04:00 +0300

    Add Angular Initial Files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBmhPQOeyCn8ph3T9pTc-oU9DJ2ij-7T8Q",
+    authDomain: "easy-bot-unloaded.firebaseapp.com",
+    databaseURL: "https://easy-bot-unloaded.firebaseio.com",
+    projectId: "easy-bot-unloaded",
+    storageBucket: "easy-bot-unloaded.appspot.com",
+    messagingSenderId: "109728863465",
+    appId: "1:109728863465:web:c6e3c5f5dafb23ff"
+  }
+};
